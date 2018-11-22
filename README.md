4a2a9368c053a7f0ae0332b7b3511d335107a099
Boriphuth Saensukphattraka <cisdbori@th.ibm.com>
2018-11-22 16:24:46 +0700

    add new test result

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # TDDdotnetCore
 Practice TDD code coverage test and .editorconfig for reference.
 
-
+# Editorconfig usage
 ![editorconfig usage](editorconfig_usage.png)
+
+# Test result
+![Test result](test_result.png)
--- TDDDemoAppTests/TextAnalyzerUnitTests.cs
+++ TDDDemoAppTests/TextAnalyzerUnitTests.cs
@@ -1,24 +1,19 @@
 using tdddemoapp;
 using Xunit;
 
-namespace TDDDemoAppTests
-{
-    public class TextAnalyzerUnitTests
-    {
+namespace TDDDemoAppTests {
+    public class TextAnalyzerUnitTests {
         [Fact]
-        public void GetTotalWordCount_WhenPassedRegularText_ShouldReturnExpectedCount()
-        {
+        public void GetTotalWordCount_WhenPassedRegularText_ShouldReturnExpectedCount () {
             // arrange
             const string textToAnalyze = @"xUnit.net is a free, open source, community-focused unit testing tool for the .NET Framework.";
-            var analyzer = new TextAnalyzer();
+            var analyzer = new TextAnalyzer ();
 
             // act
-            var count = analyzer.GetTotalWordCount(textToAnalyze);
+            var count = analyzer.GetTotalWordCount (textToAnalyze);
 
             // assert
-            Assert.Equal(14,count);
+            Assert.Equal (14, count);
         }
     }
 }
-
-
